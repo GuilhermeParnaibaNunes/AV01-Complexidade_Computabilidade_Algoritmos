05ce9bc4c16195921bf06bdb7752e31c713bd27d
tassotanouss <168739058+tassotanouss@users.noreply.github.com>
2026-09-22 21:28:39 -0300

    Refactor funcao4_assimetricos for sum and factorial
    Refactor funcao4_assimetricos to calculate a sum based on even and odd values. Even values are summed directly, while odd values compute their factorial.

--- funcao4.c
+++ funcao4.c
@@ -1,15 +1,21 @@
 #include "funcao4.h"
 
 int funcao4_assimetricos(int n, int *vetor) {
-    if (n <= 1) {
-        return 1; 
-    }
+    int somatorio = 0;
 
-    for (int i = 0; i < n - 1; i++) {
-        if (vetor[i] > vetor[i + 1]) {
-            return 0; 
+    for (int i = 0; i < n; i++) {
+        if (vetor[i] % 2 == 0) {
+            // Caminho barato: apenas soma o valor atual
+            somatorio += vetor[i];
+        } else {
+            // Caminho caro: calcula o fatorial de vetor[i]
+            int fatorial = 1;
+            for (int j = 2; j <= vetor[i]; j++) {
+                fatorial *= j;
+            }
+            somatorio += fatorial;
         }
     }
 
-    return 1; 
+    return somatorio;
 }
